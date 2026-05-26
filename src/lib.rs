baa120ea94b73c3c1170657b285df7ea7007fc8e
Travis Brown <travisrobertbrown@gmail.com>
2026-05-26 16:50:18 +0200

    Support optionality represented by an empty string

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, rust_2018_idioms)]
 #![allow(clippy::missing_errors_doc)]
 #![forbid(unsafe_code)]
+pub mod empty_string_or;
 pub mod integer_or_integer_str;
 pub mod integer_or_integer_str_array;
 pub mod integer_str;
